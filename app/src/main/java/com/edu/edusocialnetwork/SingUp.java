ce22363eb16896e2c59fd90b247655782039c4c5
octopus <generpurpo@gmail.com>
2020-04-18 18:47:10 +0300

    Added user input for saving parse objects...

--- app/src/main/java/com/edu/edusocialnetwork/SingUp.java
+++ app/src/main/java/com/edu/edusocialnetwork/SingUp.java
@@ -4,20 +4,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.SaveCallback;
 
-public class SingUp extends AppCompatActivity {
+public class SingUp extends AppCompatActivity  implements View.OnClickListener {
+
+    private TextView edtFirstName, edtSecondName, edtAge, edtGender;
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtFirstName = findViewById(R.id.edtFirstName);
+        edtSecondName = findViewById(R.id.edtSecondName);
+        edtAge = findViewById(R.id.edtAge);
+        edtGender = findViewById(R.id.edtGender);
+
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        ParseObject userSave = new ParseObject("UserSave");
+        userSave.put("FirstName", edtFirstName.getText().toString());
+        userSave.put("SecondName", edtSecondName.getText().toString());
+        userSave.put("Age", edtAge.getText().toString());
+        userSave.put("Gender", edtGender.getText().toString());
+        userSave.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Toast.makeText(SingUp.this, "UserSave saved successfully", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+/*
     public void helloClicked(View v) {
         ParseObject testSave = new ParseObject("TestSave");
         testSave.put("UserName", "user");
@@ -41,4 +73,5 @@ public class SingUp extends AppCompatActivity {
             }
         });
     }
+*/
 }
