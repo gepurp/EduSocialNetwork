b84210a3ce0709bd32f844e4762fee4b320c2c7d
octopus <generpurpo@gmail.com>
2020-04-18 18:13:22 +0300

    Configured parse server settings and made some test saves...

--- app/src/main/java/com/edu/edusocialnetwork/App.java
+++ app/src/main/java/com/edu/edusocialnetwork/App.java
@@ -2,5 +2,19 @@ package com.edu.edusocialnetwork;
 
 import android.app.Application;
 
+import com.parse.Parse;
+
 public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("ebn8XasSQJiIts1qTx4RWxCumCUpVNpw9dtqf4eM")
+                // if defined
+                .clientKey("IpiSs0jHuIE7KnGmPZTSZX51AD7aofaepWkF9tZ6")
+                .server("https://parseapi.back4app.com/")
+                .build()
+        );
+    }
 }
--- app/src/main/java/com/edu/edusocialnetwork/SingUp.java
+++ app/src/main/java/com/edu/edusocialnetwork/SingUp.java
@@ -3,6 +3,12 @@ package com.edu.edusocialnetwork;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
 public class SingUp extends AppCompatActivity {
 
@@ -10,5 +16,29 @@ public class SingUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void helloClicked(View v) {
+        ParseObject testSave = new ParseObject("TestSave");
+        testSave.put("UserName", "user");
+        testSave.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Toast.makeText(SingUp.this, "Object 'TestSave' saved successfully", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        ParseObject testSave1 = new ParseObject("TestSave1");
+        testSave1.put("UserName", "user1");
+        testSave1.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Toast.makeText(SingUp.this, "Object 'TestSave1' saved successfully", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
