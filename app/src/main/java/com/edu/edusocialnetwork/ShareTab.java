d3f8766371aa12d4dbde525b47d34f7c38ac94ae
octopus <generpurpo@gmail.com>
2020-04-23 10:00:43 +0300

    Added functionality for picking the image from user's storage...

--- app/src/main/java/com/edu/edusocialnetwork/ShareTab.java
+++ app/src/main/java/com/edu/edusocialnetwork/ShareTab.java
@@ -1,20 +1,33 @@
 package com.edu.edusocialnetwork;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.biometrics.BiometricManager;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 
 /**
@@ -75,6 +88,64 @@ public class ShareTab extends Fragment implements View.OnClickListener {
     }
 
     private void getImageForShare() {
-        // Here I'll put some code for picking the image from user storage...
+        /*
+        FancyToast.makeText(getContext(),
+                "Now you can pick the image...",
+                Toast.LENGTH_LONG,
+                FancyToast.SUCCESS,
+                true).show();
+
+        */
+
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 2000);
+    }
+
+    // Method for checking the permission request result
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1000) {
+            if (grantResults.length > 0 &&
+                    grantResults[0] ==
+                            PackageManager.PERMISSION_GRANTED) {
+
+                getImageForShare();
+            }
+        }
+    }
+
+    // ??? I don't really now how to use this method
+    // ??? Just trying to find some complete examples of the implementation
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 2000) {
+            if (resultCode == Activity.RESULT_OK) {
+                try {
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                    Cursor cursor = getActivity().getContentResolver().query(selectedImage,
+                            filePathColumn, null, null, null);
+
+                    cursor.moveToFirst();
+
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    String picturePath = cursor.getString(columnIndex);
+
+                    cursor.close();
+
+                    Bitmap receivedImageBitmap = BitmapFactory.decodeFile(picturePath);
+
+                    imgHolder.setImageBitmap(receivedImageBitmap);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
